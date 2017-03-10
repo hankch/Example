29497600bf047d3ea685114076cbd9ae2049f0af
Hank Chin <hank.chin@live.com>
2017-03-10 15:48:45 -0800

    Test Xamarin simple app

---
+++ TestXamSimple/TestXamSimple/DetailViewController.cs
@@ -0,0 +1,57 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file=DetailViewController.cs company="Micro Focus">
+// // 	Copyright (c) 2016- Micro Focus
+// // </copyright>
+// // <summary>
+// //	Author : Hank Chin
+// // 	Short description
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+using System;
+
+using UIKit;
+
+namespace TestXamSimple
+{
+	public partial class DetailViewController : UIViewController
+	{
+		public object DetailItem { get; set; }
+
+		protected DetailViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public void SetDetailItem(object newDetailItem)
+		{
+			if (DetailItem != newDetailItem)
+			{
+				DetailItem = newDetailItem;
+
+				// Update the view
+				ConfigureView();
+			}
+		}
+
+		void ConfigureView()
+		{
+			// Update the user interface for the detail item
+			if (IsViewLoaded && DetailItem != null)
+				detailDescriptionLabel.Text = DetailItem.ToString();
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			ConfigureView();
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ TestXamSimple/TestXamSimpleUITest/Main.cs
@@ -0,0 +1,24 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file=Main.cs company="Micro Focus">
+// // 	Copyright (c) 2016- Micro Focus
+// // </copyright>
+// // <summary>
+// //	Author : Hank Chin
+// // 	Short description
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+using UIKit;
+
+namespace TestXamSimpleUITest
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+	}
+}
